d8950d2036e657a140e662cef595241ba0004b14
Sean Harmon <seanisloveseanislife@gmail.com>
2026-08-12 22:48:37 -0700

    Brute Force Scraping
    Sequentially scraping pages works.
    Improvements can be made on parallel scraping and more precise wait times for data loading. This method also does not make full use of the 25 database queries which could be made quicker. Using some sort of checklist (index array) it can be determined exactly where a driver spends its queries and how many are useful (new queries) vs duplicate (transitive) queries to move up 10 indices.

--- ucsd_authenticate.py
+++ ucsd_authenticate.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import math
 import time
 
 
@@ -38,7 +39,7 @@ def search_all(driver):
     search_button = driver.find_element(By.ID, "MainContent_ContinueButton")
     search_button.click()
 
-def get_page_href(pid):
+def get_page_href(pid, max_pid):
     """
     Args:
         pid: 1-indexed
@@ -66,13 +67,100 @@ def get_page_href(pid):
     # 30 - 22, 40 - 42
     # 31 - 24, 41 - 44
 
+    # pid=122 with max_pid=127
+    # ctl_num = (144-22)+4 = 126
+    # ctl_num = (126-6)%20 + 6 = 6
+    # => ctl_num = 6
+    # shift = (130 - 127) * 2 = 6
+    # => ctl_num = 12
+
     ctl_num = 2*pid
     if pid > 11:
         ctl_num = (ctl_num - 22) + 4 # account for leading ... and re-reading ctl04, which was already shown from the prior ctl24
 
         ctl_num = (ctl_num - 6) % 20 + 6 # iterate on trailing ...
 
+
+    shift = 10 * math.ceil(max_pid / 10) - max_pid
+    if pid > 11 and max_pid - pid < 10 - shift - 1:
+        full_shift = shift * 2
+        print(f"full_shift = {full_shift}")
+        print(f"ctl_num originally = {ctl_num}")
+        ctl_num = (ctl_num+full_shift-6) % 20 + 6
+        print(f"ctl_num now = {ctl_num}")
+
     return rf"javascript:__doPostBack('ctl00$MainContent$ResultRadGrid$ctl00$ctl03$ctl01$ctl{ctl_num:02}','')"
+
+def get_offset(page_start: int, page_end:int) -> int:
+    """
+    Skips the lists of pages which do not contain page_start or page_end
+
+        Args:
+        page_start: start page number (1-indexed)
+        page_end: end page number (1-indexed)
+                  page_end - page_start <= 10 when page_end >11
+
+    Returns:
+        offset: integer number of pages to skip (0-indexed)
+    """
+    if page_start <= page_end <= 11:
+        return 0
+
+    offset = math.ceil((page_end - 11) / 10) + 1 # not in first page of 11
+
+    return offset
+
+    
+
+def get_page_list(page_start: int, page_end: int, max_page: int, driver):
+    """
+    Retrieve a set of pages based on absolute starting and ending page numbers
+
+    Args:
+        page_start: start page number (1-indexed)
+        page_end: end page number (1-indexed)
+                  Assumes page_start and page_end are properly assigned when passed
+
+    Returns:
+        pages_subset: a dictionary containing up to 11 page numbers
+    """
+    # offset can be functionalized
+    offset = get_offset(page_start, page_end)
+
+    # go to new page list
+    for list_pid in range(offset):
+        if list_pid == 0:
+            curr_page_href = get_page_href(10, max_page) # no leading previous page element on first list
+        else:
+            curr_page_href = get_page_href(10*list_pid+1, max_page) # offset 1 to account for leading previous page list `...`
+
+        page_field = driver.find_element(By.XPATH, f"""//a[@href="{curr_page_href}"]""")
+        page_field.click()
+
+        time.sleep(5)
+
+    pages_subset = dict()
+
+    # scrape page list
+    for pid in range(page_start, page_end+1):
+        curr_page_href = get_page_href(pid, max_page)
+        print(f"Visiting page: {pid}. href = {curr_page_href}")
+        # save current page
+        page_field = driver.find_element(By.ID, "ctl00_MainContent_ResultRadGrid_ctl00")
+        page_content = page_field.get_attribute("innerHTML")
+        pages_subset[pid] = page_content
+
+        # update to next page
+        next_page_field = driver.find_element(By.XPATH, f"""//a[@href="{curr_page_href}"]""")
+        try:
+            next_page_field.click()
+            # url doesn't change, but data takes a few seconds to update
+            # try replacing with implicit wait 
+            time.sleep(5) 
+        except Exception as e:
+            print(e)
+
+    return pages_subset
 
 
 def main(UCSD_USERNAME, UCSD_PASSWORD, options=Options()):
@@ -93,12 +181,8 @@ def main(UCSD_USERNAME, UCSD_PASSWORD, options=Options()):
 
         # Pull data for all pages
         pages = dict()
-        pid = 1
 
         # page number
-        curr_page_href = get_page_href(pid)
-
-        
         pages_field = driver.find_element(By.CSS_SELECTOR, ".rgWrap.rgInfoPart")
         pages_html = pages_field.get_attribute("innerHTML")
         pages_soup = BeautifulSoup(pages_html)
@@ -109,25 +193,29 @@ def main(UCSD_USERNAME, UCSD_PASSWORD, options=Options()):
             print("Failed to parse number of pages")
             raise e
 
-        while pid < num_pages:
-            print(f"Visiting page: {pid}. href = {curr_page_href}")
-            # save current page
-            page_field = driver.find_element(By.ID, "ctl00_MainContent_ResultRadGrid_ctl00")
-            page_content = page_field.get_attribute("innerHTML")
-            pages[pid] = page_content
+        print(f"There are a total of {num_pages} pages")
+        num_page_seq = get_offset(0, num_pages)
+        print(f"There will be {num_page_seq} page lists")
+        for page_seq in range(0,num_page_seq):
+            # Restart 25 viewings counter in new window
+            driver.switch_to.new_window('tab')
+            driver.get(url)
+            time.sleep(5)
+            search_all(driver)
+            time.sleep(5)
 
-            #update to next page
-            pid += 1
-            curr_page_href = get_page_href(pid)
-            next_page_field = driver.find_element(By.XPATH, f"""//a[@href="{curr_page_href}"]""")
-            try:
-                next_page_field.click()
-                # url doesn't change, but data takes a few seconds to update
-                # try replacing with implicit wait 
-                time.sleep(5) 
-            except Exception as e:
-                print(e)
+            if page_seq == 0:
+                if num_pages <= 11:
+                    start, end = 1, num_pages
+                else:
+                    start, end = 1, 11
+            else:
+                start, end = 10*page_seq + 2, min(10*page_seq + 11, num_pages)
 
+            print(f"Start = {start}, End = {end}")
+            curr_page_list = get_page_list(start, end, num_pages, driver)
+            # pages have unique identifiers so update is lossless
+            pages.update(curr_page_list)
 
         # save all pages
         with open("data/raw.json", "w") as f:
